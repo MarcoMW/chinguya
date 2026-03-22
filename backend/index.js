b0df51f193101f6509b38f197cc606369ab3816e
MarcoMW <mwahnschafft@gmail.com>
2026-03-22 13:40:38 +0100

    Fix Express routing wildcard path-to-regexp TypeError

--- backend/index.js
+++ backend/index.js
@@ -248,7 +248,7 @@ io.on('connection', (socket) => {
   });
 });
 
-app.get('*', (req, res) => {
+app.get(/^.*$/, (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
